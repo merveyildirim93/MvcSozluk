f7a7e6385851c23c5bae0d93c55d07198088f23a
Merve Yıldırım <merveyildirimm93@gmail.com>
2021-05-20 05:07:15 +0300

    -

---
+++ MvcSozlukUI/Controllers/AdminHeadingController.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Entity;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSozlukUI.Controllers
+{
+    public class AdminHeadingController : Controller
+    {
+        // GET: AdminHeading
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var heading = headingManager.GetList();
+            return View(heading);
+        }
+
+        [HttpGet]
+        public ActionResult addHeading()
+        {
+            List<SelectListItem> categoryList = (from x in categoryManager.GetList()
+                                                 select new SelectListItem
+                                                 {
+                                                     Text = x.CategoryName,
+                                                     Value = x.CategoryId.ToString()
+                                                 }).ToList();
+            ViewBag.categories = categoryList;
+
+            List<SelectListItem> writerList = (from x in writerManager.GetList()
+                                                 select new SelectListItem
+                                                 {
+                                                     Text = x.WriterName,
+                                                     Value = x.WriterId.ToString()
+                                                 }).ToList();
+            ViewBag.writers = writerList;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+                headingManager.HeadingAdd(heading);
+                return RedirectToAction("Index");
+        }
+    }
+}
